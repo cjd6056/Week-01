b6c041ef3b570d6b491d166a913bf3db26ed0926
cjd6056 <cjd6056@psu.edu>
2024-09-09 16:48:11 -0400

    Staged Commit

--- hello wisconsin wk1 prtpation.cpp
+++
@@ -1,9 +0,0 @@
-// hello wisconsin code to be pushed to Conor Dowdell's repository 06 SEP 2024 for week 1 participation aersp 424
-
-#include <iostream>
-using namespace std;
-int main()
-{
-	cout << "Hello Wisconsin" << '\n' << '\n' << "Get it?" << endl;
-	return 0;
-}
