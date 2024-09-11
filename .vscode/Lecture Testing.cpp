d10533059b973a58a4c45db5def0be66ac04f06e
cjd6056 <cjd6056@psu.edu>
2024-09-11 16:14:57 -0400

    9/11 Lecture

--- .vscode/Lecture Testing.cpp
+++ .vscode/Lecture Testing.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
+#include <array>
+#include <algorithm>
 
-int main ()
+int main()
 {
-    auto a = 1, b = 2, c = 3;
-    auto d = 4, e = 5;
-    auto f = "6";  // f is a const char*
-
-    auto g = a + b + c + d + e + f;  // f is treated as an integer (pointer arithmetic)
-
-    std::cout << g << std::endl;  // Print the result of the addition
-    std::cout << "Type of g: " << typeid(g).name() << std::endl; // Print the type information
-
+    std::array<double, 5> arr = { 98,1,72,49 };
+    std::sort( arr.begin(), arr.end() );
+    arr[3] = 6;
+    arr.at( 2 ) = 0;
+    std::replace( arr.begin(), arr.end(), 1, 11 );
+    for ( auto a : arr )
+    {
+        std::cout << a * 2 + 1 << std::endl;
+    }
     return 0;
 }
