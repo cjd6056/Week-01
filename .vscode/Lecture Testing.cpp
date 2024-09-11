f2682f79e943e7c97ba05a697af4b76f3b515914
cjd6056 <cjd6056@psu.edu>
2024-09-11 14:58:17 -0400

    Organizing...

--- .vscode/Lecture Testing.cpp
+++ .vscode/Lecture Testing.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 
-int main() {
-    // Write C++ code here
-    std::cout << "Hello World";
+int main ()
+{
+    auto a = 1, b = 2, c = 3;
+    auto d = 4, e = 5;
+    auto f = "6";  // f is a const char*
+
+    auto g = a + b + c + d + e + f;  // f is treated as an integer (pointer arithmetic)
+
+    std::cout << g << std::endl;  // Print the result of the addition
+    std::cout << "Type of g: " << typeid(g).name() << std::endl; // Print the type information
 
     return 0;
 }
